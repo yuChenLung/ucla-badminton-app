e935d15d07f188fb740eefbdb2d6148a2793fddd
Andrew Wang <andrewang2004@gmail.com>
2023-11-14 17:36:40 -0800

    added 6 courts idk format

---
+++ src/pages/Reservation.js
@@ -0,0 +1,16 @@
+import './Reservation.css'  
+
+export default function Reservation() {
+  return (
+    <body>
+      <div class="flexbox">
+        <div>Court 1</div>
+        <div>Court 2</div>
+        <div>Court 3</div>
+        <div>Court 4</div>
+        <div>Court 5</div>
+        <div>Court 6</div>
+      </div>
+    </body>
+  )
+}
